6310885b292cebbe8c2e63ef7b26054d571131d7
jphan9 <jphan9@gmail.com>
2014-08-23 20:14:10 -0700

    Added UI and implemented openFileDialog.

--- CSharpExcelAPplication/CSharpExcelAPplication/MainWindow.xaml.cs
+++ CSharpExcelAPplication/CSharpExcelAPplication/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace CSharpExcelAPplication
 {
     /// <summary>
@@ -23,6 +24,36 @@ namespace CSharpExcelAPplication
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+
+        public String openFileDialogBox()
+        {
+            String fileName; 
+            //create an instance of the open file dialog box.
+            Microsoft.Win32.OpenFileDialog openfileDialog1 = new Microsoft.Win32.OpenFileDialog();
+
+            bool? userClickedOK = openfileDialog1.ShowDialog();
+
+            //process input if the user clicked ok.
+            if (userClickedOK == true)
+            {
+                fileName = openfileDialog1.FileName;
+                return fileName;
+            }
+
+            return "No File Selected";
+        }
+
+        private void heatmapLayoutbutton_Click(object sender, RoutedEventArgs e)
+        {
+            String fileName;
+            fileName = openFileDialogBox();
+            heatmapLayoutTextbox.Text = fileName;
         }
     }
 }
